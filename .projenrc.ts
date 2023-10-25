c7424de14f34c2e53ccc8aa22966c9c71304aa09
Vincent De Smet <vincent@goodnotesapp.com>
2023-10-25 13:40:57 +0700

    fix: Use projen escape hatch to add packages read permission
    - The node-project Build Workflow component does not expose permissions configuration
    - The node-project scopedPackageOptions only supports CodeArtifact
    
    Use escape hatch to patch packages permission into build worflow.

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,4 +1,4 @@
-import { cdk8s, TextFile } from "projen";
+import { cdk8s, TextFile, JsonPatch } from "projen";
 const project = new cdk8s.Cdk8sTypeScriptApp({
   cdk8sVersion: "2.3.33",
   k8sMinorVersion: 25, // CDK8s+ version
@@ -7,9 +7,28 @@ const project = new cdk8s.Cdk8sTypeScriptApp({
   projenrcTs: true,
   prettier: true,
   deps: ["@vincentgna/cdk8s-datadog"],
+  workflowBootstrapSteps: [
+    {
+      run: 'echo "//npm.pkg.github.com/:_authToken=${{ secrets.GITHUB_TOKEN }}" > ~/.npmrc',
+    },
+  ],
 });
 project.npmrc.addRegistry("https://npm.pkg.github.com/", "@vincentgna");
 new TextFile(project, ".nvmrc", {
   lines: ["v18"],
 });
+// projen escape hatch
+// only CodeArtifact support by `scopedPackagesOptions`
+const buildWorkflowYaml = project.tryFindObjectFile(
+  ".github/workflows/build.yml",
+);
+buildWorkflowYaml?.patch(
+  JsonPatch.add("/jobs/build/permissions/packages", "read"),
+);
+const upgradeWorkflowYaml = project.tryFindObjectFile(
+  ".github/workflows/upgrade.yml",
+);
+upgradeWorkflowYaml?.patch(
+  JsonPatch.add("/jobs/upgrade/permissions/packages", "read"),
+);
 project.synth();
